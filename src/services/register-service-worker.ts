327429828ca4975c22fa946b8651924c49ae4da3
Erik Vullings <erik.vullings@gmail.com>
2022-08-24 21:15:24 +0200

    FIX URL

--- src/services/register-service-worker.ts
+++ src/services/register-service-worker.ts
@@ -38,7 +38,7 @@ export function registerServiceWorker(config?: ServiceWorkerConfiguration) {
     }
 
     window.addEventListener('load', () => {
-      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+      const swUrl = `${process.env.PUBLIC_URL}service-worker.js`;
 
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
