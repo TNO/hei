fcf23008573f2154f56ca7c69b40999d1e58fc60
Erik Vullings <erik.vullings@gmail.com>
2022-09-08 12:34:55 +0200

    When no model is loaded, highlight what to do in about

--- src/components/about-page.ts
+++ src/components/about-page.ts
@@ -4,7 +4,7 @@ import { render } from 'mithril-ui-form';
 import { Dashboards } from '../models';
 import { MeiosisComponent } from '../services';
 
-const md = `#### About
+const md = (highlight = false) => `#### About
 
 This website offers an overview of state-of-the-art interventions to enhance human performance. In order to use it, contact TNO to obtain the JSON database file describing all interventions.
 
@@ -16,11 +16,13 @@ This platform is intended for informational purposes only and it does not provid
 
 ###### Ministry of Defence employee
 
-In order to use this platform, you will need to upload a configuration file (JSON) on the home page. You can request the latest version of this configuration file by contacting the HCSE program leader. Without this file, there will be no interventions displayed on the platform.
+<p class="${
+  highlight ? 'red-text' : ''
+}">In order to use this platform, you will need to upload a configuration file (JSON) on the home page. You can request the latest version of this configuration file by contacting the HCSE (Human Capability & Survivability Enhancement) program leader, [Olaf Binsch](mailto:olaf.binsch@tno.nl). Without this file, there will be no interventions displayed on the platform.</p>
 
 You can use this platform to browse through the collection of intervention technologies on the overview page. You can use filters in the Advanced Search bar to specify what you are looking for in an intervention.
 
-By selecting ‘Compare’, recognizable by the <i class="material-icons">balance</i> icon, for different intervention technologies, you can view them alongside each other on the Compare page. Bookmarking an intervention allows you to find them more easily next time you visit the platform. If you have any questions about the HCSE (Human Capability & Survivability Enhancement) interventions, you can contact the expert that is listed at the bottom of each intervention page.
+By selecting ‘Compare’, recognizable by the <i class="material-icons">balance</i> icon, for different intervention technologies, you can view them alongside each other on the Compare page. Bookmarking an intervention allows you to find them more easily next time you visit the platform. If you have any questions about the HCSE interventions, you can contact the expert that is listed at the bottom of each intervention page.
 
 ###### TNO researcher
 
@@ -42,10 +44,11 @@ export const AboutPage: MeiosisComponent = () => {
     }) => setPage(Dashboards.ABOUT),
     view: ({
       attrs: {
-        state: { curUser },
+        state: { curUser, model },
         actions: { saveCurUser },
       },
     }) => {
+      const isCleared = !model || !model.interventions || model.interventions.length === 0;
       if (!curUser) saveCurUser('mod');
       return [
         m('.row', [
@@ -74,7 +77,7 @@ export const AboutPage: MeiosisComponent = () => {
           //   className: 'col s6',
           // }),
         ]),
-        m('.row', m.trust(render(md))),
+        m('.row', m.trust(render(md(isCleared)))),
       ];
     },
   };
