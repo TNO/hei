70051abd80fa5d5fc40d168bbfb62e9ea7ecd077
Erik Vullings <erik.vullings@tno.nl>
2023-06-29 14:46:36 +0200

    :lipstick:

--- src/assets/images.ts
+++ src/assets/images.ts
@@ -2,5 +2,11 @@ import pharma from '../assets/pharma.webp';
 import nutrition from '../assets/nutrition.webp';
 import beverage from '../assets/beverage.webp';
 
-export const resolveImg = (url: string, img: string) =>
-  url === 'pharma' ? pharma : url === 'nutrition' ? nutrition : url === 'beverage' ? beverage : img;
+export const resolveImg = (label: string, img: string) =>
+  label === 'pharma'
+    ? pharma
+    : label === 'nutrition'
+    ? nutrition
+    : label === 'beverage'
+    ? beverage
+    : img;
