53e06c84ed165fd228b5a4508fc443279f4c6824
Erik Vullings <erik.vullings@tno.nl>
2023-11-02 15:26:35 +0100

    Update login (as admin)

--- src/components/about-page.ts
+++ src/components/about-page.ts
@@ -16,17 +16,16 @@ This website offers an overview of state-of-the-art interventions to enhance hum
 
 ###### Ministry of Defence employee
 
-<p class="${
-  highlight ? 'red-text' : ''
-}">In order to use this platform, you will need to upload a configuration file (JSON) on the home page. You can request the latest version of this configuration file by contacting the HCSE (Human Capability & Survivability Enhancement) program leader, [Olaf Binsch](mailto:olaf.binsch@tno.nl). Without this file, there will be no interventions displayed on the platform.</p>
+<p class="${highlight ? 'red-text' : ''
+  }">In order to use this platform, you will need to upload a configuration file (JSON) on the home page. You can request the latest version of this configuration file by contacting the HCSE (Human Capability & Survivability Enhancement) program leader, [Olaf Binsch](mailto:olaf.binsch@tno.nl). Without this file, there will be no interventions displayed on the platform.</p>
 
 You can use this platform to browse through the collection of intervention technologies on the overview page. You can use filters in the Advanced Search bar to specify what you are looking for in an intervention.
 
 By selecting ‘Compare’, recognizable by the <i class="material-icons">balance</i> icon, for different intervention technologies, you can view them alongside each other on the Compare page. Bookmarking an intervention allows you to find them more easily next time you visit the platform. If you have any questions about the HCSE interventions, you can contact the expert that is listed at the bottom of each intervention page.
 
-###### TNO researcher
+###### Administrator
 
-If you are a TNO researcher and you want to contribute to the platform by adding or updating an HCSE intervention, you can change the Current user to TNO researcher. This allows you to add and change interventions using the button "Add new intervention". Remember that the changes will only be saved locally on your own PC. If you want to implement your changes in the master file, do the following:
+If you are an administrator and you want to contribute to the platform by adding or updating an HCSE intervention, you can change the current user to administrator. This allows you to add and change interventions using the button "Add new intervention". Remember that the changes will only be saved locally on your own PC. If you want to implement your changes in the master file, do the following:
 
 1. Make sure you have uploaded the latest configuration file from the HCSE sharepoint folder
 2. Implement you changes or additions to the platform
@@ -60,7 +59,7 @@ export const AboutPage: MeiosisComponent = () => {
               placeholder: 'Select user',
               options: [
                 { id: 'mod', label: 'Defence employee' },
-                { id: 'admin', label: 'TNO researcher' },
+                { id: 'admin', label: 'Administrator' },
               ],
               // data: users.reduce((acc, cur) => {
               //   acc[cur.name] = cur.url || null;
